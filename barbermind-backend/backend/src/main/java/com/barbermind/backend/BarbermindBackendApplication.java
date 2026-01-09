a81d23f64f6bc648eff154ee3b04f673c1ce152e
Eduardo Ballesteros Pérez <ballesterospereze@gmail.com>
2026-01-09 12:19:13 +0100

    Initial commit

---
+++ barbermind-backend/backend/src/main/java/com/barbermind/backend/BarbermindBackendApplication.java
@@ -0,0 +1,13 @@
+package com.barbermind.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbermindBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbermindBackendApplication.class, args);
+	}
+
+}
