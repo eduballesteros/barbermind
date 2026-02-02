8f904aec046a4131b61069a60e331fda8249552d
Eduardo Ballesteros Pérez <ballesterospereze@gmail.com>
2026-02-02 12:02:56 +0100

    feat: Implementar la funcionalidad de creación de citas con mapeo de estados y persistencia.

--- barbermind-backend/backend/src/main/java/com/barbermind/backend/booking/infrastructure/adapter/in/controller/AppointmentController.java
+++ barbermind-backend/backend/src/main/java/com/barbermind/backend/booking/infrastructure/adapter/in/controller/AppointmentController.java
@@ -1,4 +1,31 @@
 package com.barbermind.backend.booking.infrastructure.adapter.in.controller;
 
+import com.barbermind.backend.booking.application.dto.CreateAppointmentCommand;
+import com.barbermind.backend.booking.domain.port.in.CreateAppointmentUseCase;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/appointments")
+@RequiredArgsConstructor
 public class AppointmentController {
+
+    private final CreateAppointmentUseCase createAppointmentUseCase;
+
+    @PostMapping
+    public ResponseEntity<UUID> createAppointment(@Valid @RequestBody CreateAppointmentCommand command){
+        UUID id = createAppointmentUseCase.createAppointment(command);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(id);
+    }
+
 }
---
+++ barbermind-backend/backend/src/test/java/com/barbermind/backend/booking/domain/model/AppointmentTest.java
@@ -0,0 +1,1 @@
+package com.barbermind.backend.booking.domain.model;
